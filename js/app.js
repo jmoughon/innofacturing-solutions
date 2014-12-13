031e01a0f259b1e9a404be56272dcb177d0f74c4
James <james@astonishdesign.com>
2014-12-13 16:58:44 -0600

    Add all content and images
    Signed-off-by: James <james@astonishdesign.com>

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(window).scroll(function(e){
+  parallax();
+});
+
+function parallax(){
+  var scrolled = $(window).scrollTop();
+  $('.billboard').css('background-position','center ' + -(scrolled*0.15)+'px');
+}
