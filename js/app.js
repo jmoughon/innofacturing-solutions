c520737b248b771dea4a2c6932ef2aaf151eb74f
James <james@astonishdesign.com>
2015-01-03 10:21:44 -0600

    correct email and phone js
    Signed-off-by: James <james@astonishdesign.com>

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 //$(document).foundation();
+var email = new Array('.com','factur','gerard','o.orti','z@inno','ingsol','utions'),
+    phone = new Array('71','2','+','1','979','55','95');
+
+$('.email').append("<a target='_blank' href='mailto:"+email[2]+email[3]+email[4]+email[1]+email[5]+email[6]+email[0]+"'>"+email[2]+email[3]+email[4]+email[1]+email[5]+email[6]+email[0]+"</a>");
+$('.email-button').append("<a class='button' target='_blank' href='mailto:"+email[2]+email[3]+email[4]+email[1]+email[5]+email[6]+email[0]+"'>Connect With Us</a>");
+$('.phone').append("<a target='_blank' href='tel:"+phone[2]+phone[3]+phone[4]+phone[1]+phone[5]+phone[6]+phone[0]+"'>"+phone[2]+phone[3]+' ('+phone[4]+') '+phone[1]+phone[5]+'-'+phone[6]+phone[0]+"</a>");
 
 if ($("html").hasClass("cssfilters")) {
   $window = $(window);
